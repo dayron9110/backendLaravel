c799a973696cb07174c1115ec99130229ccd7325
dayron91 <dayroncediel@gmail.com>
2017-10-17 08:04:20 -0500

    initial commit

---
+++ app/Http/Controllers/Users.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class Users extends Controller {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index($id = null) {
+        if ($id == null) {
+            return User::orderBy('id', 'asc')->get();
+        } else {
+            return $this->show($id);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request) {
+        $user = new User;
+
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = $request->input('password');
+        $user->save();
+
+        return 'User record successfully created with id ' . $user->id;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id) {
+        return User::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id) {
+        $user = User::find($id);
+
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = $request->input('password');
+        $user->save();
+
+        return "Sucess updating user #" . $user->id;
+    }
+
+    /**
+     * Remove the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id) {
+        $user = User::find($id);
+        $user->delete();
+
+        return "User record successfully deleted #";
+    }
+}
